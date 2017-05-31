99d60793689ec4e0d516ba6ffeaab63bc77867fb
Nic Swart <Nic@cerebri.com>
2017-05-31 12:41:44 +0200

    Add new pre scripts

--- HDP_Scripts/pre_ambari.sh
+++ HDP_Scripts/pre_ambari.sh
@@ -9,8 +9,7 @@ cd devops_scripts
 chmod -R -v u+x ./*
 
 # Install Java and Anaconda
-sudo ./CENTOS_Scripts/centos_install_java.sh
-sudo ./CENTOS_Scripts/centos_install_anaconda.sh
+#sudo ./CENTOS_Scripts/centos_install_java.sh
 
 # Install Cassandra service and Datastax repo
 sudo ./HDP_Scripts/ambari_install_cassandra_service.sh
--- HDP_Scripts/pre_hdp.sh
+++ HDP_Scripts/pre_hdp.sh
@@ -9,8 +9,7 @@ cd devops_scripts
 chmod -R -v u+x ./*
 
 # Install Java and Anaconda
-./CENTOS_Scripts/centos_install_java.sh
-./CENTOS_Scripts/centos_install_anaconda.sh
+#./CENTOS_Scripts/centos_install_java.sh
 
 # Install Datastax repo
 ./HDP_Scripts/hdp_install_datastax_repo.sh
