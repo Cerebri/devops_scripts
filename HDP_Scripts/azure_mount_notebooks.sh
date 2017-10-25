fef04de18af7929b0cdf4ca6aca7f4b30817339f
Nic Swart <Nic@cerebri.com>
2017-10-25 12:15:40 +0200

    Adding Azure mount

--- HDP_Scripts/azure_mount_notebooks.sh
+++ HDP_Scripts/azure_mount_notebooks.sh
@@ -10,7 +10,7 @@ if [[ ! -d "/azure_notebooks" ]]; then
 	mkdir -p /azure_notebooks
 
 	# Mount directories here
-	mount -t cifs //$1.file.core.windows.net/$share /azure_notebooks -o vers=3.0,username=$1,password=$2,dir_mode=0555,file_mode=0444,uid=$3,gid=$3
+	mount -t cifs //$1.file.core.windows.net/$share /azure_notebooks -o vers=3.0,username=$1,password=$2,dir_mode=0777,file_mode=0444,uid=$3,gid=$3
 	echo "//$1.file.core.windows.net/$share    /azure_notebooks    cifs    vers=3.0,username=$1,password=$2,dir_mode=0555,file_mode=0444,uid=$3,gid=$3 0 0" >> /etc/fstab
 
 fi
