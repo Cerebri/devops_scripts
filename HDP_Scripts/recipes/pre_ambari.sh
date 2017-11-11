c126ea36b84e81b1886e85cbe93d6d3d04c24d3b
Nic Swart <Nic@cerebri.com>
2017-11-11 14:39:53 +0100

    changed

--- HDP_Scripts/recipes/pre_ambari.sh
+++ HDP_Scripts/recipes/pre_ambari.sh
@@ -5,20 +5,14 @@
 # Typically it will be run by the Cloudbreak deployer
 #
 
-cd /tmp
-
 # Get scripts
+cd /tmp
 sudo yum install git -y
 git clone https://github.com/Cerebri/devops_scripts.git
-cd devops_scripts
 
-# Install Java and Anaconda
-#sudo ./CENTOS_Scripts/centos_install_java.sh
-
-# Install Cassandra & ELK service and Datastax repo
-sudo bash ./HDP_Scripts/ambari_install_elk_service.sh
-sudo bash ./HDP_Scripts/ambari_install_cassandra_service.sh
-sudo bash ./HDP_Scripts/ambari_install_postgres_service.sh
-sudo bash ./HDP_Scripts/hdp_install_repos.sh
-sudo bash ./HDP_Scripts/recipes/post_anaconda.sh
-sudo bash ./HDP_Scripts/recipes/post_file_mapping.sh
+sudo bash /tmp/devops_scripts/HDP_Scripts/ambari_install_elk_service.sh
+sudo bash /tmp/devops_scripts/HDP_Scripts/ambari_install_cassandra_service.sh
+sudo bash /tmp/devops_scripts/HDP_Scripts/ambari_install_postgres_service.sh
+sudo bash /tmp/devops_scripts/HDP_Scripts/hdp_install_repos.sh
+sudo bash /tmp/devops_scripts/HDP_Scripts/recipes/post_anaconda.sh
+sudo bash /tmp/devops_scripts/HDP_Scripts/recipes/post_file_mapping.sh
--- HDP_Scripts/recipes/pre_hdp.sh
+++ HDP_Scripts/recipes/pre_hdp.sh
@@ -5,17 +5,11 @@
 # Typically it will be run by the Cloudbreak deployer
 #
 
-cd /tmp
-
 # Get scripts
+cd /tmp
 sudo yum install git -y
 git clone https://github.com/Cerebri/devops_scripts.git
-cd devops_scripts
 
-# Install Java and Anaconda
-#./CENTOS_Scripts/centos_install_java.sh
-
-# Install Datastax repo
-sudo bash ./HDP_Scripts/hdp_install_repos.sh
-sudo bash ./HDP_Scripts/recipes/post_anaconda.sh
-sudo bash ./HDP_Scripts/recipes/post_file_mapping.sh
+sudo bash /tmp/devops_scripts/HDP_Scripts/hdp_install_repos.sh
+sudo bash /tmp/devops_scripts/HDP_Scripts/recipes/post_anaconda.sh
+sudo bash /tmp/devops_scripts/HDP_Scripts/recipes/post_file_mapping.sh
