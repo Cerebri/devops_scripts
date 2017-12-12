870a1285804a8d8f6e6de208c56a0acd00c3e7f6
Nic Swart <Nic@cerebri.com>
2017-12-12 16:01:20 +0100

    Adding pfSense azure setup

--- AZURE_Scripts/waagent.sh
+++ AZURE_Scripts/waagent.sh
@@ -6,7 +6,7 @@ name=waagent
 waagent_flags="--daemon"
 
 command="/usr/sbin/${name}"
-command_interpreter="/usr/bin/python"
+command_interpreter="python"
 
 load_rc_config $name
 run_rc_command "$1"
