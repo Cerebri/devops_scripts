f419238650fa1d2b571328fe090ed55f6e9797dc
Jesse Macht <jesse@cerebri.com>
2017-12-14 16:23:35 -0500

    Updating install Java script to pre-accept the licensing agreement

--- JUPYTER_Scripts/install_java8.sh
+++ JUPYTER_Scripts/install_java8.sh
@@ -4,6 +4,8 @@
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:webupd8team/java -y 
 sudo apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get install oracle-java8-installer -y
 
 sudo echo 'export JAVA_HOME="/usr/bin/java"' >> /etc/profile
