186c4fcf61935630b6c22369850e4a16913501a0
Nic Swart <Nic@cerebri.com>
2018-02-26 15:14:10 +0100

    Adding alerts upload

--- HDP_Scripts/alerts/upload_alert.sh
+++ HDP_Scripts/alerts/upload_alert.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Call function with admin password
+# Call function with admin password, ambari IP and clustername
 
-AMBARI_HOST=localhost
+AMBARI_HOST=$2
 
-curl -u admin:${1} -i -k -H 'X-Requested-By:ambari' -X POST -d @allocated_memory.json  https://${AMBARI_HOST}/ambari/api/v1/clusters/autoscaling/alert_definitions
-curl -u admin:${1} -i -k -H 'X-Requested-By:ambari' -X POST -d @pending_apps.json  https://${AMBARI_HOST}/ambari/api/v1/clusters/autoscaling/alert_definitions
-curl -u admin:${1} -i -k -H 'X-Requested-By:ambari' -X POST -d @pending_containers.json  https://${AMBARI_HOST}/ambari/api/v1/clusters/autoscaling/alert_definitions
+curl -u admin:${1} -i -k -H 'X-Requested-By:ambari' -X POST -d @allocated_memory.json  https://${AMBARI_HOST}/ambari/api/v1/clusters/$3/alert_definitions
+curl -u admin:${1} -i -k -H 'X-Requested-By:ambari' -X POST -d @pending_apps.json  https://${AMBARI_HOST}/ambari/api/v1/clusters/$3/alert_definitions
+curl -u admin:${1} -i -k -H 'X-Requested-By:ambari' -X POST -d @pending_containers.json  https://${AMBARI_HOST}/ambari/api/v1/clusters/$3/alert_definitions
