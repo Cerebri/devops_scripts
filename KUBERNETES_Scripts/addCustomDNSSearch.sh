5bdf55b762132b806a34ac7ce5d5bcff49b1d8fe
Nic Swart <nic@cerebri.com>
2019-02-22 14:23:56 +0100

    Add k8s Custom Domain

---
+++ KUBERNETES_Scripts/addCustomDNSSearch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Change the reddog.microsoft.com to Custom Domain
+
+DOMAINNAME=$1
+
+echo $(date) " - Starting Script"
+
+sed -i .bak 's/reddog.microsoft.com/${DOMAINNAME}/' /etc/resolv.conf
