4b8e57e78dea6cf3ee0a59ed7925c60698cb0b3d
Phani kumar <34389835+ultrakumar@users.noreply.github.com>
2019-02-26 09:32:16 -0500

    Modifying the old script

---
+++ JUPYTER_Scripts/jupyter_node_file_mapping_modified_old.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Storage Account ID
+export ACCOUNT_NAME=$1
+# Storage Account key
+export ACCOUNT_PASSWORD=$2
+
+apt-get install samba-client samba-common cifs-utils -y
+
+mkdir -p /data-old
+mkdir -p /scratch-old
+mkdir -p /jupyter-notebooks-old
+
+# Mount directories here
+
+mount -t cifs //$ACCOUNT_NAME.file.core.windows.net/sftpdata /data-old -o vers=3.0,username=$ACCOUNT_NAME,password=$ACCOUNT_PASSWORD,dir_mode=0555,file_mode=0444,uid=dev,gid=dev
+echo "//$ACCOUNT_NAME.file.core.windows.net/sftpdata    /data-old    cifs    vers=3.0,username=$ACCOUNT_NAME,password=$ACCOUNT_PASSWORD,dir_mode=0555,file_mode=0444,uid=dev,gid=dev 0 0" >> /etc/fstab
+
+mount -t cifs //$ACCOUNT_NAME.file.core.windows.net/scratch /scratch-old -o vers=3.0,username=$ACCOUNT_NAME,password=$ACCOUNT_PASSWORD,dir_mode=0777,file_mode=0777,uid=dev,gid=dev
+echo "//$ACCOUNT_NAME.file.core.windows.net/scratch    /scratch-old    cifs    vers=3.0,username=$ACCOUNT_NAME,password=$ACCOUNT_PASSWORD,dir_mode=0777,file_mode=0777,uid=dev,gid=dev 0 0" >> /etc/fstab
+
+mount -t cifs //$ACCOUNT_NAME.file.core.windows.net/jupyter-notebooks /jupyter-notebooks-old -o vers=3.0,username=$ACCOUNT_NAME,password=$ACCOUNT_PASSWORD,dir_mode=0777,file_mode=0777,uid=dev,gid=dev
+echo "//$ACCOUNT_NAME.file.core.windows.net/jupyter-notebooks    /jupyter-notebooks-old    cifs    vers=3.0,username=$ACCOUNT_NAME,password=$ACCOUNT_PASSWORD,dir_mode=0777,file_mode=0777,uid=dev,gid=dev 0 0" >> /etc/fstab
+
+exit 0
