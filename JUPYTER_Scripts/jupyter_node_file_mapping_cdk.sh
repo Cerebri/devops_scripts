3c1f345ffafbf164f162bac8d4abc45e502767e9
Phani kumar <34389835+ultrakumar@users.noreply.github.com>
2019-08-23 16:40:48 -0400

    Created a new script for the cdk

---
+++ JUPYTER_Scripts/jupyter_node_file_mapping_cdk.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Storage Account ID
+export ACCOUNT_ID=$1
+# Storage Account key
+export ACCOUNT_KEY=$2
+
+apt-get install samba-client samba-common cifs-utils -y
+
+mkdir -p /data
+mkdir -p /scratch
+mkdir -p /jupyter-notebooks
+
+# Mount directories here
+mount -t cifs //$ACCOUNT_ID.file.core.windows.net/sftpdata-decrypted /data -o vers=3.0,username=$ACCOUNT_ID,password=$ACCOUNT_KEY,dir_mode=0555,file_mode=0444,uid=dev,gid=dev
+echo "//$ACCOUNT_ID.file.core.windows.net/sftpdata-decrypted    /data    cifs    vers=3.0,username=$ACCOUNT_ID,password=$ACCOUNT_KEY,dir_mode=0555,file_mode=0444,uid=dev,gid=dev 0 0" >> /etc/fstab
+
+mount -t cifs //$ACCOUNT_ID.file.core.windows.net/scratch /scratch -o vers=3.0,username=$ACCOUNT_ID,password=$ACCOUNT_KEY,dir_mode=0777,file_mode=0777,uid=dev,gid=dev
+echo "//$ACCOUNT_ID.file.core.windows.net/scratch    /scratch    cifs    vers=3.0,username=$ACCOUNT_ID,password=$ACCOUNT_KEY,dir_mode=0777,file_mode=0777,uid=dev,gid=dev 0 0" >> /etc/fstab
+
+mount -t cifs //$ACCOUNT_ID.file.core.windows.net/jupyter-notebooks /jupyter-notebooks -o vers=3.0,username=$ACCOUNT_ID,password=$ACCOUNT_KEY,dir_mode=0777,file_mode=0777,uid=dev,gid=dev
+echo "//$ACCOUNT_ID.file.core.windows.net/jupyter-notebooks    /jupyter-notebooks    cifs    vers=3.0,username=$ACCOUNT_ID,password=$ACCOUNT_KEY,dir_mode=0777,file_mode=0777,uid=dev,gid=dev 0 0" >> /etc/fstab
+
+exit 0
